685b2f67452151dadcf3ca85d0a316e719018ad1
removedporn <86824510+removedporn@users.noreply.github.com>
2022-10-03 09:39:59 +0800

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// Import function to create a topohelper class
-export { fromTopojson as from } from './format/fromTopojson.js'
-export { fromGeojson } from './format/fromGeojson.js'
-
-export { view } from './view.js'
-
-
-
